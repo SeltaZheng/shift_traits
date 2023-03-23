0f9453f593bd65b39fa6b62489e8aae69bc2c04a
Ting Zheng <selta43@gmail.com>
2023-03-23 13:41:06 -0500

    sample selection script finished

--- LeafSample/SampleSelection.py
+++ LeafSample/SampleSelection.py
@@ -7,6 +7,7 @@ Li, Wenze, et al. "HSPXY: A hybrid‐correlation and diversity‐distances based
 '''
 
 
+import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from scipy.spatial.distance import cdist
@@ -155,6 +156,135 @@ def max_min_distance_split(distance, train_size):
     return select_pts, remaining_pts
 
 
+## main:
+dir_spec = r'D:\GoogleDrive\Projects_ongoing\shift\data\spectra\OD'
+dir_t = r'D:\GoogleDrive\Projects_ongoing\shift\data\traits'
+dir_out =r'D:\GoogleDrive\Projects_ongoing\shift\data\meta'
+
+df_t = pd.read_csv(f'{dir_t}/shift_DS_Predict_NEON_v3.csv')
+df_spec = pd.read_csv(f'{dir_spec}/ovendried_spectra_sample_mean.csv')
+
+## parse the sample ID, exclude NPVs
+dir_in = r'D:\GoogleDrive\Projects_ongoing\shift\data\meta'
+# df_sample = pd.read_csv(f'{dir_in}/processed/sample_list_simplified.csv')
+df_iform = pd.read_csv(f'{dir_in}/processed/sample_list_simplified_ORNL_v3.csv', encoding='unicode_escape')
+df_inv = pd.read_csv(f'{dir_in}/processed/SHIFT_sample_inventory_20230308.csv')
+# Clean the df_inv
+df_inv = df_inv[df_inv['Sample Number'] != '131_flower']
+df_inv = df_inv[df_inv['Sample Number'] != '161_S'].reset_index(drop=True)
+n_inv = df_inv['Sample Number'].unique().astype(int)
+n_inv_OD = df_inv.loc[(df_inv['Type']=='OD') | (df_inv['Type']=='BK'), 'Sample Number'].unique().astype(int)
+# match the spec sample with inv
+sample_n = df_spec['sample_ID'].str.split('_').str.get(1).astype(int)
+diff_spec2inv = set(sample_n).difference(n_inv) # {1824, 1678, 531, 1689, 1693, 1694}
+diff_inv2spec = set(n_inv_OD).difference(sample_n) # { 551, 40, 1601, 71, 174, 1776, 755,  1859, 335, 336, 346,  374, 376,  449, 451, 1012}
+
+# filter out NPV based on df_iform
+spec_meta = pd.DataFrame(df_spec['sample_ID'])
+spec_meta['Sample Number'] = sample_n
+NPV = df_iform.loc[(df_iform['Species or type'] == 'NPV') & (df_iform['Sample Taken?'] == 'Yes'), 'Sample Number']
+# filter out flower/seeds/full senescence samples
+# clean the phenophase column
+df_iform['Pheno'] = df_iform['Phenophase (if rare flowers or seeds, add as multi-select - if sampling flowers separately, add new entry)'].str.split(',').str.get(0)
+dropV = df_iform.loc[(df_iform['Pheno'] == 'Flowers') | (df_iform['Pheno'] == 'Full senescence') | (df_iform['Pheno'] == 'Seeds'), 'Sample Number']
+# drop nan and convert to int
+NPV = list(NPV[~NPV.isna()].astype(int))
+# find bulk from inventory
+BK = df_inv.loc[df_inv['Type']=='BK', 'Sample Number'].unique().astype(int)
+# no scan list from Natalie
+df_noScan = pd.read_csv(f'{dir_out}/Lists_from_others/sample_no_LMA_scans.csv')
+noScan = df_noScan['no scans'].astype(int)
+
+drop_ls = NPV + list(dropV[~dropV.isna()].astype(int)) + list(BK) + list(noScan)
+idx = ~spec_meta['Sample Number'].isin(drop_ls)
+spec_meta_veg = spec_meta[idx].reset_index(drop=True)
+spec_veg = df_spec[idx].iloc[:, 1:].values
 
 
+# select and match the corresponding traits
+trait_ls = ['Cellulose',  'Fiber', 'Lignin', 'Nitrogen', 'Calcium', 'NSC',  'Phenolics']
+trait_m = [x + '_M' for x in trait_ls]
 
+traits = df_t.loc[idx, trait_m].values
+
+# select 200 samples
+ratio = 1 - 200/spec_veg.shape[0]
+
+x_train_index, x_test_index = kennardstone(spec_veg, test_size=ratio)
+
+spec_ls = spec_meta_veg.iloc[x_train_index, :]
+
+
+# combine spec and traits
+# remove the traits record with nan
+idx_t = ~np.isnan(traits).any(axis=1)
+ratio = 1 - 200/spec_veg[idx_t, :].shape[0]
+x_train_index, x_test_index = spxy(spec_veg[idx_t, :], traits[idx_t, :], test_size=ratio)
+
+spec_meta_veg_sub = spec_meta_veg[idx_t].reset_index(drop=True)
+spec_t_ls = spec_meta_veg_sub.iloc[x_train_index, :]
+common_n = set(spec_ls['Sample Number']).intersection(set(spec_t_ls['Sample Number']))
+common_ls = spec_ls[spec_ls['Sample Number'].isin(common_n)]
+# perform join to get more info
+
+spec_ls_meta = spec_ls.merge(df_iform, how='left', on='Sample Number')
+spec_t_ls_meta = spec_t_ls.merge(df_iform, how='left', on='Sample Number')
+common_ls_meta = common_ls.merge(df_iform, how='left', on='Sample Number')
+
+# add more info from Elsa and wetland
+df_elsa = pd.read_csv(f'{dir_out}/Lists_from_others/SHIFT_UCLA_JPL_Sample_IDs.csv')
+df_wetland = pd.read_csv(f'{dir_out}/Lists_from_others/SHIFT_wetland_samples_Silva.csv')
+df_wetland.rename(columns={'sample_id': 'Sample Number'}, inplace=True)
+df_elsa.rename(columns={'JPL_sample_ID': 'Sample Number'}, inplace=True)
+
+spec_ls_meta = spec_ls_meta.merge(df_elsa.loc[:, ['Sample Number', 'species_code']].drop_duplicates(), how='left', on='Sample Number')
+spec_ls_meta = spec_ls_meta.merge(df_wetland.loc[:, ['Sample Number', 'species']].drop_duplicates(), how='left', on='Sample Number')
+
+spec_t_ls_meta = spec_t_ls_meta.merge(df_elsa.loc[:, ['Sample Number', 'species_code']].drop_duplicates(), how='left', on='Sample Number')
+spec_t_ls_meta = spec_t_ls_meta.merge(df_wetland.loc[:, ['Sample Number', 'species']].drop_duplicates(), how='left', on='Sample Number')
+
+common_ls_meta = common_ls_meta.merge(df_elsa.loc[:, ['Sample Number', 'species_code']].drop_duplicates(), how='left', on='Sample Number')
+common_ls_meta = common_ls_meta.merge(df_wetland.loc[:, ['Sample Number', 'species']].drop_duplicates(), how='left', on='Sample Number')
+
+spec_ls_meta.to_csv(f'{dir_out}/selected_sample_list_based_on_dryspec_v2.csv', index=False)
+spec_t_ls_meta.to_csv(f'{dir_out}/selected_sample_list_based_on_dryspec_traits_v2.csv', index=False)
+common_ls_meta.to_csv(f'{dir_out}/selected_sample_list_in_common_v2.csv', index=False)
+
+#%%----------------- With the final list, check if the sample has both OD and FF parts-------------------------------
+dir_in =r'D:\GoogleDrive\Projects_ongoing\shift\data\meta'
+df_od_final = pd.read_csv(f'{dir_out}/selected_samples_final_correctdate.csv')
+df_inv = pd.read_csv(f'{dir_in}/processed/SHIFT_sample_inventory_20230308.csv')
+# Clean the df_inv
+df_inv = df_inv[df_inv['Sample Number'] != '131_flower']
+df_inv = df_inv[df_inv['Sample Number'] != '161_S'].reset_index(drop=True)
+
+# FF sample list from df_inv
+n_ff = df_inv.loc[df_inv['Type']=='FF', 'Sample Number'].astype(int)
+n_od_final = df_od_final['sample number'].astype(int)
+n_common = set(n_od_final).intersection(n_ff)
+n_dif = set(n_od_final).difference(n_ff)
+df_common = df_od_final[df_od_final['sample number'].isin(n_common)]
+df_common.to_csv(f'{dir_out}/selected_sample_final_wt_od_ff.csv', index=False)
+pd.DataFrame(n_dif).to_csv(f'{dir_out}/no_FF_samples.csv')
+
+# Overall comparison
+df_iform = pd.read_csv(f'{dir_in}/processed/sample_list_simplified_ORNL_v3.csv', encoding='unicode_escape')
+NPV_BK = df_iform.loc[((df_iform['Species or type'] == 'NPV') | (df_iform['Species or type'] == 'Bulk sample')) & (df_iform['Sample Taken?'] == 'Yes'), 'Sample Number']
+NPV_BK = list(NPV_BK[~NPV_BK.isna()].astype(int))
+
+# only keep od and FF from inventory
+df_inv = df_inv[(df_inv['Type']=='OD') | (df_inv['Type']=='FF')]
+# drop NPV from inventory
+idx = ~df_inv['Sample Number'].astype(int).isin(NPV_BK)
+df_inv = df_inv[idx]
+
+n_od = df_inv.loc[df_inv['Type']=='OD', 'Sample Number'].unique().astype(int)
+n_ff = df_inv.loc[df_inv['Type']=='FF', 'Sample Number'].unique().astype(int)
+
+n_com = set(n_ff).intersection(set(n_od))
+n_ff_only = set(n_ff).difference(set(n_od))
+n_od_only = set(n_od).difference(set(n_ff))
+
+pd.DataFrame(n_ff_only).to_csv(f'{dir_in}/UWM_inv_sort/FF_only_in_inv_exclude_NPV_BK.csv', index=False)
+pd.DataFrame(n_od_only).to_csv(f'{dir_in}/UWM_inv_sort/OD_only_in_inv_exclude_NPV_BK.csv', index=False)
+pd.DataFrame(n_com).to_csv(f'{dir_in}/UWM_inv_sort/OD_and_FF_in_inv_exclude_NPV_BK.csv', index=False)
